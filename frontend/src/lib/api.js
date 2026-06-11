4f758649b40c0c1596da1df69e1c2f8ce199bacf
Ehab Naser <ehabtt289@gmail.com>
2026-06-11 10:52:09 +0300

    Update api.js

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_URL || "http://localhost:2567";
+const baseURL =  "https://video-save-ot64.onrender.com";
 export const api = axios.create({ baseURL });
 
 export async function listFiles() {
